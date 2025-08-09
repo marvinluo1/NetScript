b2242a958e2a75bf88df91494d627077beb4475a
Marvin <live9080@gmail.com>
2025-08-09 22:31:06 +0800

    Update convert.js

--- iptv/convert.js
+++ iptv/convert.js
@@ -5,15 +5,19 @@ trList.forEach(tr => {
     const pName = tdList[1].innerText
     if (!pName.includes('画中画') && !pName.includes('单音轨')) {
         pp.push({
-            name: tdList[1].innerText,
-            url: tdList[2].innerText
+            name: tdList[1].innerText.replace(/高清$/, ''),
+            url: tdList[2].innerText,
+            backUrl: tdList[6].innerText,
         })
     }
 })
 
 var text = pp.map(p => {
     return `${p.name},http://192.168.100.1:10000/rtp/${p.url}`
-}
-).join('\n');
+}).join('\n');
+var playbackText = pp.map(p => {
+    return `${p.name},${p.backUrl}`
+}).join('\n');
 
-console.log(text);
+
+console.log(text, playbackText);
